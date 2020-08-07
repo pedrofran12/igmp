075e04c7cce868b9a3d98b42fc940921a78a904a
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2020-08-07 19:14:50 +0100

    add latest code updates from HPIM-DM/PIM-DM implementations

---
+++ igmp/__init__.py
@@ -0,0 +1,1 @@
+from igmp.InterfaceIGMP import InterfaceIGMP
---
+++ igmp/utils.py
@@ -0,0 +1,21 @@
+import array
+
+
+def checksum(pkt: bytes) -> int:
+    """
+    Calculate checksum from a bunch of bytes
+    """
+    if len(pkt) % 2 == 1:
+        pkt += "\0"
+    s = sum(array.array("H", pkt))
+    s = (s >> 16) + (s & 0xffff)
+    s += s >> 16
+    s = ~s
+    return (((s >> 8) & 0xff) | s << 8) & 0xffff
+
+
+# obtain TYPE_CHECKING (for type hinting)
+try:
+    from typing import TYPE_CHECKING
+except ImportError:
+    TYPE_CHECKING = False
