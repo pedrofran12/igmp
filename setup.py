6b757ec9629386a8200f3ca5acb4a563e94d026d
Pedro Oliveira <pedrofran12@users.noreply.github.com>
2020-08-07 22:21:36 +0100

    IGMPv2 Implementation (#4)
    * add latest code updates from HPIM-DM/PIM-DM implementations

---
+++ setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import setup, find_packages
+
+# we only support Python 3 version >= 3.2
+if len(sys.argv) >= 2 and sys.argv[1] == "install" and sys.version_info < (3, 2):
+    raise SystemExit("Python 3.2 or higher is required")
+
+
+dependencies = open("requirements.txt", "r").read().splitlines()
+setup(
+    name="igmp",
+    description="IGMPv2 router-side protocol implementation for Python",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    keywords="IGMP IGMPv2 Router IPv4 Multicast Interest",
+    version="1.0.2",
+    url="http://github.com/pedrofran12/igmp",
+    author="Pedro Oliveira",
+    author_email="pedro.francisco.oliveira@tecnico.ulisboa.pt",
+    license="MIT",
+    install_requires=dependencies,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Information Technology",
+        "Topic :: System :: Networking",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.2",
+)
